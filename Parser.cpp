d31feffd5223dffb43cdbd5f2002df10349f02d6
hwi-hyat <prespektrejp@gmail.com>
2023-08-26 22:15:50 +0900

    edited parser.cpp for PING cmd and added gitignore file

--- Parser.cpp
+++ Parser.cpp
@@ -170,7 +170,17 @@ void Parser::cmd_nick()
 
 void Parser::cmd_ping()
 {
-
+    if (this->_tokens.size() < 2)
+        this->_server.send_message_to_client_with_code(this->_client, "461", "PING :Not enough parameters");
+	else if (this->_tokens.size() == 2)
+        this->_server.send_message_to_fd(this->_client.getFd(), ":PONG " + this->_server.getHostname() + " " + this->_client.getHostname() +  "\r\n");
+	// else
+	// {
+	// 	string args;
+	// 	for (unsigned int i = 0; i < c.getArgs().size(); i++)
+	// 		args += c.getArgs()[i] + " ";
+	// 	sendResponse("PONG :message from " + hostname + " " + args, user);
+	// }
 }
 
 void Parser::cmd_user()
